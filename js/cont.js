8eee404dc076df1fe54cab11955dc249ebafbef7
Namehwang <54699767+Namehwang@users.noreply.github.com>
2020-11-30 15:40:33 +0900

    1.1

--- js/cont.js
+++ js/cont.js
@@ -14,21 +14,12 @@ const op3 = document.querySelector('.cont4 .project');
 const cl3 = document.querySelector('.cont4 .close');
 const cont4 = document.querySelector('.cont4 .wrap');
 
-const op4 = document.querySelector('.cont5 .project');
-const cl4 = document.querySelector('.cont5 .close');
-const cont5 = document.querySelector('.cont5 .wrap');
-
-const op5 = document.querySelector('.cont6 .project');
-const cl5 = document.querySelector('.cont6 .close');
-const cont6 = document.querySelector('.cont6 .wrap');
-
 op.addEventListener('click', () => {
     cont1.classList.toggle('open');
-    
 });
 
 cl.addEventListener('click', () => {
-    cont1.classList.toggle('open')
+    cont1.classList.toggle('close')
 });
 
 op1.addEventListener('click', () => {
@@ -36,7 +27,7 @@ op1.addEventListener('click', () => {
 });
 
 cl1.addEventListener('click', () => {
-    cont2.classList.toggle('open');
+    cont2.classList.toggle('close')
 });
 
 op2.addEventListener('click', () => {
@@ -44,7 +35,7 @@ op2.addEventListener('click', () => {
 });
 
 cl2.addEventListener('click', () => {
-    cont3.classList.toggle('open');
+    cont3.classList.toggle('close')
 });
 
 op3.addEventListener('click', () => {
@@ -52,21 +43,5 @@ op3.addEventListener('click', () => {
 });
 
 cl3.addEventListener('click', () => {
-    cont4.classList.toggle('open');
-});
-
-op4.addEventListener('click', () => {
-    cont5.classList.toggle('open');
-});
-
-cl4.addEventListener('click', () => {
-    cont5.classList.toggle('open')
-});
-
-op5.addEventListener('click', () => {
-    cont6.classList.toggle('open');
-});
-
-cl5.addEventListener('click', () => {
-    cont6.classList.toggle('open')
+    cont4.classList.toggle('close')
 });
